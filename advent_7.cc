68a27d52179a31d6ef0a70fc1dd61431332c62f5
Jose Fenic Peiteado padilla <72447219+fenic-peiteado@users.noreply.github.com>
2022-12-07 13:05:00 +0000

    parte 1

---
+++ advent_7.cc
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+// advent of code day 7
+
+int main() {
+  std::ifstream input_file("input");
+  std::string line;
+  std::regex rgx("[0-9]+");
+  std::map<std::string, size_t> dir;
+  std::stack<std::string> dir_stack;
+  std::regex rgx_dir("cd");
+  std::regex rgx_ant("cd\\s..$");
+  size_t sum = 0;
+  size_t sum_no = 0;
+  std::string dir_name;
+  while (std::getline(input_file, line)) {
+    std::smatch match;
+    if (std::regex_search(line, match, rgx_dir)) {
+      if (std::regex_search(line, match, rgx_ant)) {
+        size_t aux = dir[dir_stack.top()];
+        dir_stack.pop();
+        dir[dir_stack.top()] = aux + dir[dir_stack.top()];
+        continue;
+      }
+      // Substraer el nombre del directorio con regex
+      std::regex rgx_dir_name("\\$\\scd\\s");
+      std::string direc_name = std::regex_replace(line, rgx_dir_name, "");
+      dir_name = direc_name;
+      if (dir_stack.empty()) {
+        dir[dir_name] = 0;
+      } else {
+        if (dir_stack.top() == "/") {
+          dir[dir_stack.top() + dir_name] = 0;
+          dir_name = dir_stack.top() + dir_name;
+        } else {
+          dir[dir_stack.top() + "/" + dir_name] = 0;
+          dir_name = dir_stack.top() + "/" + dir_name;
+        }
+      }
+      dir_stack.push(dir_name);
+    }
+    if (std::regex_search(line, match, rgx)) {
+      sum_no = std::stoi(match.str());
+      dir[dir_name] += sum_no;
+    }
+  }
+
+  while (!dir_stack.empty()) {
+    size_t aux = dir[dir_stack.top()];
+    dir_stack.pop();
+    if (dir_stack.empty()) {
+      break;
+    }
+    dir[dir_stack.top()] = aux + dir[dir_stack.top()];
+  }
+
+  size_t final_sum = 0;
+  for (auto const& x : dir) {
+    std::cout << x.first << " " << x.second << std::endl;
+    if (x.second <= 100000) {
+      final_sum += x.second;
+    }
+  }
+
+  std::cout << "Suma de los numeros:X " << final_sum << std::endl;
+  return 0;
+}
